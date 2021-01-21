8e145c95466193d44cebd948e011d45b0e3f819c
Sparsh-2512 <sparshkhandelwal729cd@gmail.com>
2021-01-21 14:43:18 +0530

    added search function and date and time with notes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-console.log("Welcome to magic notes");
+console.log("Welcome to NotesApp");
 showNotes();
 let addBtn = document.getElementById('addBtn');
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 // add notes
 addBtn.addEventListener('click', e => {
     let note = document.getElementById('note');
     let title = document.getElementById('title');
-    if(note.value!='' && title.value!='')
-    {
-        // a object literal containing note title and note
+    let today = new Date();
+    let todayDateString = `${today.getDate()} ${monthNames[today.getMonth()]} ${today.getFullYear()}`;
+    let todayTimeString = `${today.getHours()}:${today.getMinutes()}`;
+    if (note.value != '' && title.value != '') {
+        // a object literal containing note title and note content alongwith date&time
         let myObj = {
             noteTitle: title.value,
-            noteContent: note.value
+            noteContent: note.value,
+            date: todayDateString,
+            time: todayTimeString
         }
         let notes = localStorage.getItem("notes");
         if (notes === null) {
@@ -26,9 +33,8 @@ addBtn.addEventListener('click', e => {
         note.value = '';
         showNotes();
     }
-    else
-    {
-        alert('Enter both note and title!');
+    else {
+        alert('Enter note and title both!');
     }
 });
 
@@ -45,7 +51,9 @@ function showNotes() {
     // iterating through the notesObj array which is an array of objects(title,note), to get note title and content to use in html to be displayed
     notesObj.forEach((element, index) => {
         html +=
-            `<div class="card m-2" style="width: 18rem;">
+        `<div class="card m-2" style="width: 18rem;">
+        <span class="date">${element.date}</span>
+        <span class="time">${element.time}</span>
         <div class="card-body">
             <h5 class="card-title">${element.noteTitle}</h5>
             <p class="card-text">${element.noteContent}</p>
@@ -53,6 +61,7 @@ function showNotes() {
         </div>
         </div>`
     });
+    // ${date} ${month} ${year} ${hour} ${minute}
     let notesContainer = document.getElementById('notesContainer');
     notesContainer.innerHTML = html;
 }
@@ -70,4 +79,46 @@ function deleteNode(index) {
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
+}
+
+// searching notes
+let searchBar = document.getElementById('searchBar');
+let searchBtn = document.getElementById('searchBtn');
+searchBar.addEventListener('input', e => {
+    searchNotes();
+});
+searchBtn.addEventListener('click', e => {
+    e.preventDefault();
+    searchNotes();
+});
+
+function searchNotes() {
+    if (searchBar.value != '') {
+        let notes = localStorage.getItem("notes");
+        if (notes === null) {
+            notesObj = [];
+        }
+        else {
+            notesObj = JSON.parse(notes);
+        }
+        let newHtml = "";
+        notesObj.forEach(element => {
+            if (element.noteTitle.includes(searchBar.value) || element.noteContent.includes(searchBar.value)) {
+                newHtml +=
+                    `<div class="card m-2" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${element.noteTitle}</h5>
+                    <p class="card-text">${element.noteContent}</p>
+                    <button id="tbd" onclick="deleteNode(this.id)" class="btn btn-danger">Delete</button>
+                </div>
+                </div>`;
+                let notesContainer = document.getElementById('notesContainer');
+                notesContainer.innerHTML = '';
+                notesContainer.innerHTML = newHtml;
+            }
+        });
+    }
+    else {
+        showNotes();
+    }
 }
